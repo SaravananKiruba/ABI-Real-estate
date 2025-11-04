ef9c7d8e45752ac67713a583a04100af97094853
Saravanan kiruba S <kirubachetti@gmail.com>
2025-11-04 09:29:51 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react()],
-    base: '/ABI-Real-estate/',
+    base: '/Real-Estate-IBMS/',
     server: {
         port: 3000,
         open: true,
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ABI-Real-estate/',
+  base: '/Real-Estate-IBMS/',
   server: {
     port: 3000,
     open: true,
